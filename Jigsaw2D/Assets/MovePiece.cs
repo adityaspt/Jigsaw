a3dee5daebbd88c8865c700602dd98164f1ec722
Aditya Patil <adityaspt@gmail.com>
2020-04-12 00:10:09 +0530

    PC
    Input

--- Jigsaw2D/Assets/MovePiece.cs
+++ Jigsaw2D/Assets/MovePiece.cs
@@ -4,45 +4,99 @@ using UnityEngine;
 
 public class MovePiece : MonoBehaviour
 {
-    public string pieceStatus = "";
-    public bool pieceLocked = false;
-    public bool checkPiecePos = false;
+    private bool pieceLocked ;
+    private bool piecePickedUp;
+    private static MovePiece instance;
     // Start is called before the first frame update
     void Start()
     {
-        
+        instance = this;
+        pieceLocked = false;
+        piecePickedUp = false;
     }
 
     // Update is called once per frame
+    private void OnMouseDown()
+    {
+        piecePickedUp = true;
+    }
+    private void OnMouseUp()
+    {
+        piecePickedUp = false;
+    }
     void Update()
     {
-        if (Input.GetMouseButton(0))
+
+        if (piecePickedUp) {
+
+            Vector3 newPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = newPos;
+                }
+        if (!pieceLocked && piecePickedUp)
         {
-            checkPiecePos = true;
+            Vector2 mousePos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            Vector2 objPos = Camera.main.ScreenToWorldPoint(mousePos);
+            this.transform.position = objPos;
         }
 
-            if (!pieceLocked && checkPiecePos)
-            {
-                Vector2 mousePos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
-                Vector2 objPos = Camera.main.ScreenToWorldPoint(mousePos);
-                transform.position = objPos;
-            }
-        
-    
-        if(Input.GetMouseButtonUp(0))
-        {
-            checkPiecePos = false;
-        }
+
+        //if (Input.GetMouseButtonUp(0))
+        //{
+        //    piecePickedUp = false;
+        //}
 
 
     }
-    private void OnTriggerEnter2D(Collider2D collision)
+
+    bool DoesPieceMatchWithPlace(string a,string b)
     {
-        if (collision.gameObject.name == gameObject.name)
+        if(b.Contains(a) )
         {
-            
-            transform.position = collision.transform.position;
-            pieceLocked = true; 
+            return true;
+        }
+        else /*if(!b.Contains(a) && !b.Contains("place"))*/
+        {
+            return false;
         }
     }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+
+        if (!piecePickedUp)
+        {
+            //if (collision.gameObject.name ==gameObject.name)
+            if(DoesPieceMatchWithPlace(gameObject.name,collision.gameObject.name))
+            {
+                pieceLocked = true;
+                transform.position = collision.transform.position;
+                GetComponent<BoxCollider2D>().enabled = false;
+                collision.GetComponent<BoxCollider2D>().enabled = false;
+            }
+            else
+            {
+                //if (!piecePickedUp)
+                //{
+                    GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, .5f);
+                    transform.position = collision.transform.position;
+               // }
+               
+            }
+        }
+        else
+        {
+            GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 1f);
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (!DoesPieceMatchWithPlace(gameObject.name, collision.gameObject.name))
+        {
+
+            GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 1f);
+
+        }
+    }
+
 }
