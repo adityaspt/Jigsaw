4e5386c4f1be61d1fd67ea57ac1e764183048d34
Aditya Patil <adityaspt@gmail.com>
2020-04-18 14:50:55 +0530

    Latest pc push

---
+++ Jigsaw2D/Assets/Scripts/DragnDrog.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class DragnDrog : MonoBehaviour
+{
+    public GameObject selectedPiece;
+    int Order = 1; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+            if(hit.transform.CompareTag("Puzzle"))
+            {
+                if (!hit.transform.GetComponent<pieceScript>().inRightPos)
+                {
+                    
+                    selectedPiece = hit.transform.gameObject;
+                    selectedPiece.GetComponent<pieceScript>().isSelected = true;
+                    selectedPiece.GetComponent<SortingGroup>().sortingOrder = Order;
+                }
+            }
+            //else if(hit.transform.CompareTag("Untagged"))
+            //{
+            //    print("No piece");
+            //}
+            
+        }
+        if(Input.GetMouseButtonUp(0))
+        {
+            if (selectedPiece != null)
+            {
+                selectedPiece.GetComponent<SortingGroup>().sortingOrder = 0;
+                selectedPiece.GetComponent<pieceScript>().isSelected = false;
+                selectedPiece = null;
+                
+            }
+        }
+        if(selectedPiece !=null)
+        {
+            Vector3 newPos=(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+            selectedPiece.transform.position = new Vector3(newPos.x,newPos.y,0);
+        }
+    }
+}
---
+++ Jigsaw2D/Assets/Scripts/pieceScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pieceScript : MonoBehaviour
+{
+    public bool inRightPos = false;
+    private Vector3 rightPos;
+    public bool isSelected=false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rightPos = transform.position;
+        //transform.position = new Vector3(UnityEngine.Random.Range(7.5f, 16.5f), UnityEngine.Random.Range(-6.7f, 1.53f));
+        transform.position = new Vector3(UnityEngine.Random.Range(-0.6f, 3.5f), UnityEngine.Random.Range(-1.5f, 1.5f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(transform.position,rightPos)<0.5f)
+        {
+            if (!isSelected)
+            {
+                transform.position = rightPos;
+                inRightPos = true;
+            }
+        }
+
+
+    }
+}
