6532adb1ed0fc730eb818c4683253d900328d42c
Aditya Patil <adityaspt@gmail.com>
2020-09-01 02:19:43 +0530

    Restart Multiplayer Photon 2
    PC

---
+++ Jigsaw2D/Assets/TestMultiplayer/MenuScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+namespace TestMultiplayer
+{
+    public class MenuScript : MonoBehaviourPunCallbacks
+    {
+        bool IsConnecting;
+        public void PlayButton()
+        {
+            IsConnecting = true;
+            if (PhotonNetwork.IsConnected)
+            {
+                PhotonNetwork.JoinRandomRoom();
+            }
+            else
+            {
+                PhotonNetwork.ConnectUsingSettings();
+            }
+        }
+        private void Start()
+        {
+            PhotonNetwork.AutomaticallySyncScene = true;
+        }
+        public override void OnConnectedToMaster()
+        {
+            base.OnConnectedToMaster();
+            if (IsConnecting)
+            {
+                PhotonNetwork.JoinRandomRoom();
+            }
+        }
+        public override void OnJoinRandomFailed(short returnCode, string message)
+        {
+            base.OnJoinRandomFailed(returnCode, message);
+            PhotonNetwork.CreateRoom(null,new RoomOptions {MaxPlayers=3 });
+            IsConnecting = false;
+        }
+        public override void OnDisconnected(DisconnectCause cause)
+        {
+            base.OnDisconnected(cause);
+        }
+        public override void OnJoinedRoom()
+        {
+            base.OnJoinedRoom();
+            PhotonNetwork.LoadLevel(1);
+        }
+    }
+}
